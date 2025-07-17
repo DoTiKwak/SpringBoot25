28a34b6989ec45b5f67dbe23927ddac72eeffd8b
220-08\gusckd1009 <gusckd1009@gmail.com>
2025-07-17 16:45:37 +0900

    2.QueryDSL+Service

--- src/main/java/org/mbc/board/domain/Board.java
+++ src/main/java/org/mbc/board/domain/Board.java
@@ -3,27 +3,37 @@ package org.mbc.board.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
+@Entity // 데이터베이스 테이블 관련 객체
 @Getter
-@Builder // 빌더 패턴 setter 대신 활용
-@AllArgsConstructor // 모든 필드값으로 생성자 생성
-@NoArgsConstructor // 기본 생성자
-@ToString // 객체 주소가 아닌 값을 출력
-public class Board extends BaseEntity { // extends BaseEntity (날짜 관련 된 jpa 연결)
-
+@Builder // 빌더 패턴 세터 대신 활용
+@AllArgsConstructor // 모든 필드값으로 생성자 만듬
+@NoArgsConstructor // 기본생성자 
+@ToString   // 객체 주소가 아닌 값을 출력
+public class Board extends BaseEntity{ //  extends BaseEntity (날짜 관련된 jpa 연결)
+    
     @Id // pk로 선언용 ( notnull, unique, indexing )
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 번호 생성용
-    private Long bno;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동번호 생성용
+    private Long bno ;  // 게시물 번호
 
-    @Column(length = 500, nullable = false)
-    private String title;
+    @Column(length = 500, nullable = false) // nn
+    private String title;  //제목
 
     @Column(length = 2000, nullable = false)
-    private String content;
+    private String content; //내용 
 
     @Column(length = 50, nullable = false)
-    private String writer;
+    private String writer; // 작성자
 
+    //Hibernate:
+    //    create table board (
+    //        bno bigint not null auto_increment,
+    //        content varchar(255),
+    //        title varchar(255),
+    //        writer varchar(255),
+    //        primary key (bno)
+    //    ) engine=InnoDB
+
+    // baseEntity 상속 후 변경 코드
     //Hibernate:
     //    alter table if exists board
     //       add column moddate datetime(6)
@@ -40,11 +50,10 @@ public class Board extends BaseEntity { // extends BaseEntity (날짜 관련 된
     //    alter table if exists board
     //       modify column writer varchar(50) not null
 
-    public void change(String title, String content) {
+    public void change(String title, String content){
         // 제목과 내용만 수정하는 메서드 (세터 대체용)
         this.title = title;
         this.content = content;
-
     }
 
 }
